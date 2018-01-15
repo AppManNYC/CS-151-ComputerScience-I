279f589c2c33bceb599135b2d0941dd0b9870b71
Matthew Krieger <mattkrieger3@gmail.com>
2018-01-15 13:18:02 -0500

    Add files via upload

---
+++ Project 2/src/Type.java
@@ -0,0 +1,39 @@
+/* 
+    **********************************
+    ** Matthew Krieger              **
+    ** mattkrieger3@gmail.com       **
+    ** CS 151 / Section: 01         **
+    ** Project 01                   **
+    **********************************
+*/
+
+public class Type
+{
+
+  public static boolean isInteger(String s)
+  {  
+    try
+    {
+        Integer.parseInt(s);  
+        return true;  
+    }  
+    catch(NumberFormatException e)  
+    {  
+        return false;  
+    }  
+  }
+  
+  public static boolean isDouble(String s)
+  {  
+    try
+    {
+        Double.parseDouble(s);  
+        return true;  
+    }  
+    catch(NumberFormatException e)  
+    {  
+        return false;  
+    }  
+  }
+  
+}
